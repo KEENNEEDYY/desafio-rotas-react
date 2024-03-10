dac48b1eeab0db7d98d707902543282eee9b6c27
KEENNEEDYY <kennedy.costa.valenet@gmail.com>
2024-03-10 19:16:02 -0300

    Header
    Signed-off-by: KEENNEEDYY <kennedy.costa.valenet@gmail.com>

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,26 @@
+import { NavLink } from "react-router-dom";
+import homeImg from '../../assets/home.svg';
+
+import './styles.css';
+
 
 export default function Header() {
     return (
-        <div>
-            header
-        </div>
+        <header className="bds-header">
+            <nav className="bds-header-nav">
+                <NavLink to="/home" className={({ isActive }) => isActive ? "bds-navlink-active" : ""}>
+                    <h3>Início</h3>
+                </NavLink>
+                <NavLink to="/products" className={({ isActive }) => isActive ? "bds-navlink-active" : ""}>
+                    <h3>Produtos</h3>
+                </NavLink>
+                <NavLink to="/about" className={({ isActive }) => isActive ? "bds-navlink-active" : ""}>
+                    <h3>Sobre nós</h3>
+                </NavLink>
+            </nav>
+            <NavLink to="/home">
+                <img src={homeImg} alt="home" />
+            </NavLink>
+        </header>
     )
 }
