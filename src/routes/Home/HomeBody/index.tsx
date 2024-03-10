927f1f9c8cef7a8c2e99d3fc9ef148f07a6e48bd
KEENNEEDYY <kennedy.costa.valenet@gmail.com>
2024-03-10 19:17:04 -0300

    HomeBody
    Signed-off-by: KEENNEEDYY <kennedy.costa.valenet@gmail.com>

--- src/routes/Home/HomeBody/index.tsx
+++ src/routes/Home/HomeBody/index.tsx
@@ -1,8 +1,13 @@
 
+
+import './styles.css';
+
 export default function HomeBody() {
     return (
-        <div>
-            Home body
+        <div className="bds-homebody-container">
+            <h1>
+                Bem-vindos!
+            </h1>
         </div>
     )
 }
