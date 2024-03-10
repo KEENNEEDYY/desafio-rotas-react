17a0314e5dee8acf87e69e155f98c58eceda3e57
KEENNEEDYY <kennedy.costa.valenet@gmail.com>
2024-03-10 19:16:39 -0300

    About page
    Signed-off-by: KEENNEEDYY <kennedy.costa.valenet@gmail.com>

---
+++ src/routes/Home/About/index.tsx
@@ -0,0 +1,12 @@
+
+import './styles.css';
+
+export default function About() {
+    return (
+        <div className="bds-about-container">
+            <h1>
+                Sobre nós
+            </h1>
+        </div>
+    )
+}
