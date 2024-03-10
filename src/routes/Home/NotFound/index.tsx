a83fdaa8c8d9c8f6e3ee7be1a64a47d48dfed94f
KEENNEEDYY <kennedy.costa.valenet@gmail.com>
2024-03-10 19:17:30 -0300

    Not found page
    Signed-off-by: KEENNEEDYY <kennedy.costa.valenet@gmail.com>

---
+++ src/routes/Home/NotFound/index.tsx
@@ -0,0 +1,11 @@
+import './styles.css';
+
+export default function NotFound() {
+    return (
+        <div className="bds-not-found-container">
+            <h1>
+                Não encontrado!
+            </h1>
+        </div>
+    )
+}
